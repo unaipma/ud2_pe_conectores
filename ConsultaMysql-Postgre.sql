16d0e555744058b51dc959d21a04ad310eab8bb1
kevin <unaipastormartinez3@gmail.com>
2024-11-14 20:10:40 +0100

    consutla

---
+++ ConsultaMysql-Postgre.sql
@@ -0,0 +1,33 @@
+-- Tabla de Jugadores
+use ud2conectores;
+CREATE TABLE players (
+    player_id INT PRIMARY KEY,         -- ID único del jugador
+    experience INT NOT NULL,           -- Nivel de experiencia
+    life_level INT NOT NULL,           -- Puntos de vida del jugador
+    coins INT NOT NULL,                -- Monedas acumuladas
+    session_count INT NOT NULL,        -- Número de sesiones jugadas
+    last_login DATE                    -- Fecha de la última conexión
+);
+
+-- Tabla de Videojuegos
+CREATE TABLE games (
+    game_id INT PRIMARY KEY,           -- Identificador del juego
+    isbn VARCHAR(13) NOT NULL,         -- Identificador ISBN del juego
+    title VARCHAR(255) NOT NULL,       -- Título del videojuego
+    player_count INT NOT NULL,         -- Número de jugadores registrados
+    total_sessions INT NOT NULL,       -- Total de sesiones o partidas jugadas
+    last_session DATE                  -- Fecha de la última sesión
+);
+
+-- Tabla de Partidas
+CREATE TABLE sessions (
+    game_id INT,                       -- Identificador del juego
+    player_id INT,                     -- Identificador del jugador
+    experience INT,                    -- Incremento del nivel de experiencia
+    life_level INT,                    -- Actualización del nivel de vida (+/-)
+    coins INT,                          -- Actualización de las monedas acumuladas (+/-)
+    session_date DATE,                 -- Fecha de la sesión o partida
+    PRIMARY KEY (game_id, player_id, session_date),  -- Clave primaria compuesta
+    FOREIGN KEY (game_id) REFERENCES games(game_id),  -- Relación con la tabla juegos
+    FOREIGN KEY (player_id) REFERENCES players(player_id) -- Relación con la tabla jugadores
+);
